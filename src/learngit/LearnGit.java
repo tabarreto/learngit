5393b49c9f58c86914313bcd44cfd76eaabd4e0f
Antonio <abarreto9201@gmail.com>
2020-10-20 08:34:24 -1100

    all changed

--- src/learngit/LearnGit.java
+++ src/learngit/LearnGit.java
@@ -15,7 +15,8 @@ public class LearnGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("dfd");
+        System.out.println("Tony");
+        System.out.println("Barreto");
     }
     
 }
